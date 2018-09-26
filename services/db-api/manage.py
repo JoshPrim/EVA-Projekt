7de8b8b491152ce332b54868a9d033cb91b3f474
BartosMosch <bartos.mosch@t-online.de>
2018-09-26 11:44:39 +0200

    removed create_engine with existing db.engine.connect()

--- services/db-api/manage.py
+++ services/db-api/manage.py
@@ -20,14 +20,14 @@ def init_tables():
     print('Running through cli_init_tables')
     """ Load station master data from csv"""
     with open(os.environ.get('MASTER_STATION'), 'r') as f:
-        conn = db.create_engine('postgresql+psycopg2://postgres:postgres@station-db:5432/eva_dev').raw_connection()
+        conn = db.engine.connect().connection
         cursor = conn.cursor()
         cmd = 'COPY station(bundesland,rb,bm,bfnr,station,bfdsabk,katvst,strasse,plz,ort,aufgabenvergeber) FROM STDIN WITH (FORMAT CSV, HEADER TRUE, DELIMITER ";", ENCODING "UTF-8")'
         cursor.copy_expert(cmd, f)
         conn.commit()
 
     with open(os.environ.get('MASTER_ELEVATOR'), 'r') as f:
-        conn = db.create_engine('postgresql+psycopg2://postgres:postgres@station-db:5432/eva_dev').raw_connection()
+        conn = db.engine.connect().connection
         cursor = conn.cursor()
         cmd = 'COPY elevator(standort_equipment,technplatzbezeichng,equipment,equipmentname,ort,wirtschaftseinheit,hersteller,baujahr,antriebsart,anzahl_haltestellen,anzahl_tueren_kabine,anzahl_tueren_schacht,foerdergeschwindigkeit,foerderhoehe,lage,tragkraft,erweiterte_ortsangabe,min_tuerbreite,kabinentiefe,kabinenbreite,kabinenhoehe,tuerhohe,fabriknummer,tuerart,geokoordinaterechtswert,geokoordinatehochwert,ausftextlichebeschreibung) FROM STDIN WITH (FORMAT CSV, HEADER TRUE, DELIMITER ";", ENCODING "UTF-8")'
         cursor.copy_expert(cmd, f)
